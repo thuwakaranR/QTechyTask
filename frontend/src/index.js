4f0ca26338c663a52971f943381dab30637253fa
thuwakaranR <it21835728@my.sliit.lk>
2024-08-02 12:11:28 +0530

    Frontend Last Working Condition Commited Code Segment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import './index.css';
 import App from './App';
 import AuthProvider from './context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Import the CSS for Toastify
 
-ReactDOM.render(
+// Find the root element
+const container = document.getElementById('root');
+const root = createRoot(container); // Create a root.
+
+// Render the app
+root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
       <ToastContainer />
     </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
